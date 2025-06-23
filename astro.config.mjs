a2d20967604e65799a74702fa4a9fa767cb76fe3
clarco-mada-digital <clarco.dev@mada-digital.net>
2025-06-23 14:42:36 +0300

    Mise en place du gh-page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+// Obtenez le nom de votre dépôt GitHub depuis l'URL
+const repositoryName = 'gestion-projet';
+
 export default defineConfig({
+  site: 'https://clarco-mada-digital.github.io',
+  base: `/${repositoryName}/`,
+  output: 'static',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     })
-  ],
-  output: 'static'
+  ]
 });
